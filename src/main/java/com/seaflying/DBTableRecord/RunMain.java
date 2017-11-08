beb7d0aa9de489128a5f354dacaa1bae42cb471b
Sea Flying <seaflying@outlook.com>
2017-11-08 09:24:02 +0800

    beta4

--- src/main/java/com/seaflying/DBTableRecord/RunMain.java
+++ src/main/java/com/seaflying/DBTableRecord/RunMain.java
@@ -18,8 +18,6 @@ public class RunMain {
      */
     public static void main(String[] args)throws Exception {
         TablesIOImpl run = new TablesIOImpl();
-//        OracleTables f1 = new OracleTables();
-//        HiveTables f2 = new HiveTables();
         Map<String,ArrayList<String>> tblist = run.getTablesname();
         ExecutorService pool = Executors.newFixedThreadPool(2);
         Callable c1 = new TRWorker(tblist.get("oracle"),1);
@@ -28,7 +26,6 @@ public class RunMain {
         Future f2 = pool.submit(c2);
         run.setTabelsCount(tblist.get("pair_id"),(ArrayList<Long>)(f1.get()),(ArrayList<Long>)(f2.get()));
         pool.shutdown();
-//      run.setTabelsCount(tblist.get("pair_id"),f1.getTablesCount(tblist.get("oracle")) ,f2.getTablesCount(tblist.get("hive")) );
         run.con.close();
     }
 
